243bd464a8c8c374d56c013276f07cccc8c8083f
mernstackproject <rammohangupta1769@gmail.com>
2024-09-29 22:39:53 +0530

    changes

--- index.js
+++ index.js
@@ -12,7 +12,7 @@
 
   app.use(
     cors({
-      origin: "http://localhost:3000",
+      origin: "https://mern-auth-client-c3zo.onrender.com",
       methods: "GET,POST,PUT,DELETE",
       credentials: true,
     })
