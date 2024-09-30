038a7261589cc22391f359470299c5ecf03fdfba
mernstackproject <rammohangupta1769@gmail.com>
2024-09-30 08:04:42 +0530

    changes cors

--- index.js
+++ index.js
@@ -10,13 +10,13 @@
   const PORT = process.env.PORT || 4000;
   const app = express();
 
-  app.use(
-    cors({
-      origin: "https://mern-auth-client-peach.vercel.app",
-      methods: "GET,POST,PUT,DELETE",
-      credentials: true,
-    })
-  );
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://mern-auth-client-peach.vercel.app"); // Your frontend URL
+    res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
 
   connectDB();
   app.use(express.json());
